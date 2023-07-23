1f206f86b718708b6ae96a838ae2da4efe0d3984
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2023-07-24 05:31:45 +0700

    Init

---
+++ src/main/java/com/eltex/Main.java
@@ -0,0 +1,25 @@
+package com.eltex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final var scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму покупок за прошлый год");
+
+        final var clientYearlyPurchases = scanner.nextInt();
+        final var discount = 0.02;
+        final var discountStart = 3_000;
+
+        final double totalDiscount;
+        if (clientYearlyPurchases >= discountStart) {
+            totalDiscount = clientYearlyPurchases * discount;
+        } else {
+            totalDiscount = 0;
+        }
+
+        System.out.printf("За прошлый год вы бы сэкономили с подпиской %s ₽", totalDiscount);
+    }
+}
